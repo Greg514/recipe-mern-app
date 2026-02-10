73c4335f995df94b2ac7b4fda1e3263d5991e107
gregory <gregjeanlouis21@gmail.com>
2026-02-10 15:07:40 -0500

    resolved the and restarted the server and connnect to the db

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [username, setUsername] = useState("")
+
   return (
-    <div>Register</div>
+    <div className="max-w-md mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Register</h1>
+
+      <form>
+        <div>
+          <label className="block text-gray-700">UserName</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            className="border p-2 w-full"
+          />
+        </div>
+      </form>
+    </div>
   )
 }
 
